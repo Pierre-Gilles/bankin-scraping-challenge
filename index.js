b0a799a4c94e870e0ecc3a5e80a41d19c75dea11
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-01-18 23:08:00 +0100

    add concurrency const

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const saveJson = require('./lib/utils/saveJson');
 
 const WEBSITE_URL = `file://${__dirname}/public/index.html`;
 const JSON_DESTINATION_PATH = './data.json';
-const CONCURRENCY = 10;
+const CONCURRENCY = 20;
 
 // At the end, this will contains the array of account entries
 var listOfEntry = [];
@@ -30,9 +30,8 @@ puppeteer.launch({headless: true})
          * Batch 2 = page 500, 550, ...
          * Of course all this happens in parallel so it's pretty fast!
          * @param {*} iteration 
-         * @param {*} concurrency 
          */
-        function call(iteration, concurrency) {
+        function call(iteration) {
             numberOfJobRunning++;
 
             // we browse the page
@@ -43,7 +42,7 @@ puppeteer.launch({headless: true})
                     // if we found data inside, let's go and browse the next batch
                     if(entries.length > 0){
                         addToList(indexOfEntry, listOfEntry, entries);
-                        return call(iteration + 50*concurrency, concurrency);
+                        return call(iteration + 50*CONCURRENCY, CONCURRENCY);
                     } 
                     
                     // if not, and there is no more job running, let's close the browser 
@@ -58,8 +57,8 @@ puppeteer.launch({headless: true})
         
         // For the first batch, let's initialize an array of promises
         var promises = [];
-        for(var i  = 0; i < 10;i++) {
-            promises.push(call(i*50, 10));
+        for(var i  = 0; i < CONCURRENCY;i++) {
+            promises.push(call(i*50, CONCURRENCY));
         }
 
         return Promise.all(promises);
